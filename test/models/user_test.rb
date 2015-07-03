27498a82c7534e18af3bb0bb007ff95643f1b85d
Isaias Pomales <impomales@gmail.com>
2015-07-03 01:38:38 -0400

    make user model and secure passwords

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,62 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "testpassword", password_confirmation: "testpassword")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 26
+    assert_not @user.valid?
+  end
+ 
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-Er@foo.bar.org first.last@foo.jp alic+bob@baz.cn]
+    valid_addresses.each do |va|
+      @user.email = va
+      assert @user.valid?, "#{va.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com Foo@bar_baz.com]
+    invalid_addresses.each do |ia|
+      @user.email = ia
+      assert_not @user.valid?, "#{ia.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    # duplicate
+    dup_user = @user.dup
+    dup_user.email = @user.email.upcase
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 7
+    assert_not @user.valid?
+  end
 end
