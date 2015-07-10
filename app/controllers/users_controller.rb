5aafa74312f0f5ef89183bfd17e35d80f4204885
Isaias Pomales <impomales@gmail.com>
2015-07-10 14:46:13 -0400

    users can login/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
-    if @user.save # will update this.
+    if @user.save 
+      log_in @user
       flash[:success] = "Welcome to Fitzy's Blog"
       redirect_to @user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,17 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   # returns current user if exist.
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 end
