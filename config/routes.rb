83c5f3f95c508c12ae9b47c52cfa71b38b2518fd
Isaias Pomales <impomales@gmail.com>
2015-07-15 13:17:08 -0400

    add password reset controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   # Sessions resource
   get 'login' => 'sessions#new'
   
@@ -33,6 +37,7 @@ Rails.application.routes.draw do
   #   resources :products
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # Example resource route with options:
   #   resources :products do
   #     member do
