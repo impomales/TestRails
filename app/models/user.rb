39c83c9046a04e5fa8a5baa0d0039fe2bb594734
Isaias Pomales <impomales@gmail.com>
2015-08-04 03:13:26 -0400

    feed added on home page of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,6 +67,11 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago.to_datetime
   end
 
+  # Defines a proto-feed
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
     def downcase_email
       self.email = email.downcase
